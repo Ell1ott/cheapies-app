378892b63b470303dcc929ecff3c95d0be0f0cee
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2024-10-23 14:40:48 +1300

    Fix: updated search to work with new infinite scroll

--- app/(tabs)/searchScreen.tsx
+++ app/(tabs)/searchScreen.tsx
@@ -1,12 +1,13 @@
 import { Search } from "@/components/navigation/header/search";
 import { Item } from "@/components/Nodes/NodeItem";
 import { NodeList } from "@/components/Nodes/NodeList";
-import { getSearchUrl } from "@/utils/apiHandler/dataFetcher";
+import { fetchRoot, getSearchUrl } from "@/utils/apiHandler/dataFetcher";
 import { getNodeList } from "@/utils/apiHandler/nodeListParser";
 import { useEffect, useState } from "react";
 
 export default function SearchScreen() {
 	let [data, setData] = useState<Item[]>([]);
+	let [url, setUrl] = useState("");
 
 	// useEffect(() => {
 	// 	console.log(data);
@@ -14,9 +15,15 @@ export default function SearchScreen() {
 
 	return (
 		<>
-			<Search onSubmit={async (q) => setData(getNodeList(getSearchUrl(q)))} />
+			<Search
+				onSubmit={async (q) => {
+					const url = getSearchUrl(q);
+					setUrl(url);
+					setData(getNodeList(await fetchRoot(url), url));
+				}}
+			/>
 
-			<NodeList items={data} />
+			<NodeList category={url} />
 		</>
 	);
 }
