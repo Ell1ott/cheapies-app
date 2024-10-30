ca8bcc205751e4a1a70c59a286bdd1009f6a6eb4
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2024-10-30 15:02:28 +1300

    Feat: add a savedNodes store using Zustand

--- utils/savedNodes.ts
+++ utils/savedNodes.ts
@@ -1,21 +1,32 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-export function saveNode(nodeId: string) {
-	savedNodes.push(nodeId);
-	saveSavedNotesToStorage();
-}
-export function unsaveNode(nodeId: string) {
-	savedNodes = savedNodes.filter((id) => id !== nodeId);
-	saveSavedNotesToStorage();
+import { Item } from "@/components/Nodes/NodeItem";
+import { create } from "zustand";
+
+interface StoreState {
+	items: Item[];
+	addItem: (item: Item) => void;
+	removeItem: (nodeId: string) => void;
+	clearItems: () => void;
 }
 
-let savedNodes: string[] = [];
-// Function to save posts
-const saveSavedNotesToStorage = async () => {
-	try {
-		const jsonValue = JSON.stringify(savedNodes);
-		await AsyncStorage.setItem("@saved_posts", jsonValue);
-		console.log("Posts saved to disk");
-	} catch (error) {
-		console.log("Error saving posts:", error);
-	}
-};
+const useStore = create<StoreState>((set) => ({
+	items: [],
+
+	// Action to add an item
+	addItem: (item: Item) =>
+		set((state: StoreState) => ({
+			items: [...state.items, item],
+		})),
+
+	// Action to remove an item by index or value
+	removeItem: (nodeId: string) =>
+		set((state: StoreState) => ({
+			items: state.items.filter(({ nodeId }) => nodeId !== nodeId),
+		})),
+
+	// Action to clear all items
+	clearItems: () => set({ items: [] }),
+}));
+
+useStore.subscribe(console.log);
+
+export default useStore;
