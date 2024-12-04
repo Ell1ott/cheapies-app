bfd3764f46ccf7661312dd1ff7f58484536eec0b
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2024-12-05 10:48:59 +1300

    Feat: add react-native-windows package

---
+++ windows/CheapiesApp/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::CheapiesApp::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::CheapiesApp::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
